2d956c0d7e3e61c3f7d4c9cb18e9c05c8bb592f8
Rodrigo Maluf <rodrigomaluf@gmail.com>
2020-11-23 06:39:57 -0300

    mapeando produto

--- src/main/java/com/estudos/spring/model/Produto.java
+++ src/main/java/com/estudos/spring/model/Produto.java
@@ -2,14 +2,23 @@ package com.estudos.spring.model;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Data
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class Produto {
 
+    @Column
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "produto_seq")
+    @SequenceGenerator(name = "produto_seq",sequenceName = "produto_seq",allocationSize = 1)
     private Long id;
+
+    @Column
     private String nome;
 }
